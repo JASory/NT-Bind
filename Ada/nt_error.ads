60adb18896c81ac806fd303a0378ded2a13eda1a
Rust-CAS <jasory@rust-cas.org>
2023-01-20 13:04:49 -0700

    Delete Ada directory

--- Ada/nt_error.ads
+++
@@ -1,8 +0,0 @@
-package NT_Error
-
-  -- Due to the construction of the Rust library these exceptions are not necessarily the true reason for failure
-  -- but rather the most likely cause. Read the original source code at www.github.com/JASory/ENT
-  DNE : exception; -- Does Not Exist  
-  Interval_Exceeded : exception; -- Does Not Exist in the datatype bounds
-  Comp_Exceeded : exception;     -- May exist in the datatype bounds but intermediate computations exceeded the bounds
-end NT_Error;
